21c73ea4a9c80a85ad74480f53ccb5c1e6028422
kamykSoyDev <pankamyk11@gmail.com>
2020-04-19 22:00:41 +0200

    updated welcome layout/fixed footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,65 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .min-height {
+            min-height: 62vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 64px;
+        }
+
+        .strong {
+            font-size: 84px
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
         .site-footer
         {
             background-color:#26272b;
@@ -188,7 +247,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="py-4 min-height">
             @yield('content')
         </main>
 
